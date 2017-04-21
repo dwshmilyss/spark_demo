fa692354fe733cec0a4493d7a08973aa159790d9
unknown <10000347@yibandc.com>
2017-04-21 18:35:54 +0800

    first commit for init project

---
+++ streaming/src/main/java/com/yiban/spark/streaming/dev/SparkStreamingByKafkaDemo.scala
@@ -0,0 +1,27 @@
+package com.yiban.spark.streaming.dev
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+
+/**
+  * Created by 10000347 on 2017/4/7.
+  */
+object SparkStreamingByKafkaDemo {
+  def main(args: Array[String]) {
+    val Array(zkQuorum, group, topics, numThreads) = args
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(5))
+    ssc.checkpoint("checkpoint")
+
+    val topicMap = topics.split(",").map((_, numThreads.toInt)).toMap
+    val lines = KafkaUtils.createStream(ssc, zkQuorum, group, topicMap).map(_._2)
+    val words = lines.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L))
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(5), 2)
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
