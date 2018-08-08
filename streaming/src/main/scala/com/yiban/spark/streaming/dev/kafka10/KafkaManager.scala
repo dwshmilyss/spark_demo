b8b3b331642ca6b881be8935cadc89d52386b164
基础组-段巍巍 <dww@yiban.cn>
2018-08-08 20:54:56 +0800

    add DataBiasProcessDemo in core module

--- streaming/src/main/scala/com/yiban/spark/streaming/dev/kafka10/KafkaManager.scala
+++ streaming/src/main/scala/com/yiban/spark/streaming/dev/kafka10/KafkaManager.scala
@@ -1,5 +1,6 @@
 package com.yiban.spark.streaming.dev.kafka10
 
+import com.yiban.spark.streaming.dev.kafka10.KafkaCluster.LeaderOffset
 import kafka.common.TopicAndPartition
 import kafka.message.MessageAndMetadata
 import kafka.serializer.Decoder
